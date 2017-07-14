aa090188ad1307bdbbbfcb03c89d337c49cd6f9d
I.Inkovskii <79262501630@ya.ru>
2017-07-14 15:12:26 +0300

    refactor(brain-even) (Step 5)

--- src/gameplay.php
+++ src/gameplay.php
@@ -9,12 +9,12 @@ namespace BrainGames\gameplay;
 
 use function cli\line;
 use function cli\prompt;
-use function BrainGames\lib\buildNamespace;
-use function BrainGames\lib\buildFilePath;
-use function BrainGames\lib\normalize;
-use function BrainGames\lib\error;
 
-
+/**
+ * @param $description - Строка с описанием игры
+ * @param $question - Лямбда-функция, формирующая строку с вопросом
+ * @param $correctAnswer - Лямбда-функция, формирующая строку с правильным ответом
+ */
 function gamePlay($description, $question, $correctAnswer)
 {
     line('Welcome to the Brain Game!');
@@ -22,15 +22,12 @@ function gamePlay($description, $question, $correctAnswer)
     $name = prompt('May I have your name?');
     line("Hello, %s!" . PHP_EOL, $name);
 
-    // play game
     for ($i = 0; $i < 3; $i++) {
-        line("Question: %s", $question);
+        $quest = $question();
+        line("Question: %s", $quest);
         $answer = prompt('Your answer', 0);
-        $corAnswer = $correctAnswer($question);
-        if ($corAnswer === false) {
-            error();
-            return;
-        } elseif ($corAnswer == normalize($answer)) {
+        $corAnswer = $correctAnswer($quest);
+        if ($corAnswer == normalize($answer)) {
             line("Correct!");
         } else {
             line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $corAnswer);
@@ -43,44 +40,49 @@ function gamePlay($description, $question, $correctAnswer)
     return;
 }
 
-function run($game)
+function normalize($answer)
 {
-    $namespace = buildNamespace('', 'BrainGames', 'games', $game, '');
-    $filePath = buildFilePath('games', $game);
-    require $filePath;
-
-    $getDescription = $namespace . 'getDescription';
-    $desc = $getDescription();
-    line('Welcome to the Brain Game!');
-    line($desc . PHP_EOL);
-    $name = prompt('May I have your name?');
-    line("Hello, %s!" . PHP_EOL, $name);
-
-    // play game
-    for ($i = 1; $i < 4; $i++) {
-        $getQuestion = $namespace . 'getQuestion';
-        //TODO в getQuestion() передаётся номер текущего шага на тот случай, если для какой-то игры вопрос будет зависеть от номера шага
-        $question = $getQuestion($i);
-        if ($question === false) {
-            error();
-            return;
-        }
-        line("Question: %s", $question);
-        $answer = prompt('Your answer', 0);
-        $getCorrectAnswer = $namespace . 'getCorrectAnswer';
-        $correctAnswer = $getCorrectAnswer($question);
-        if ($correctAnswer === false) {
-            error();
-            return;
-        } elseif ($correctAnswer == normalize($answer)) {
-            line("Correct!");
-        } else {
-            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
-            line('Let\'s try again, %s!', $name);
-            return;
-        }
-    }
-
-    line('Congratulations, %s!', $name);
-    return;
+    return mb_strtolower(trim($answer));
 }
+
+//function run($game)
+//{
+//    $namespace = buildNamespace('', 'BrainGames', 'games', $game, '');
+//    $filePath = buildFilePath('games', $game);
+//    require $filePath;
+//
+//    $getDescription = $namespace . 'getDescription';
+//    $desc = $getDescription();
+//    line('Welcome to the Brain Game!');
+//    line($desc . PHP_EOL);
+//    $name = prompt('May I have your name?');
+//    line("Hello, %s!" . PHP_EOL, $name);
+//
+//    // play game
+//    for ($i = 1; $i < 4; $i++) {
+//        $getQuestion = $namespace . 'getQuestion';
+//        //TODO в getQuestion() передаётся номер текущего шага на тот случай, если для какой-то игры вопрос будет зависеть от номера шага
+//        $question = $getQuestion($i);
+//        if ($question === false) {
+//            error();
+//            return;
+//        }
+//        line("Question: %s", $question);
+//        $answer = prompt('Your answer', 0);
+//        $getCorrectAnswer = $namespace . 'getCorrectAnswer';
+//        $correctAnswer = $getCorrectAnswer($question);
+//        if ($correctAnswer === false) {
+//            error();
+//            return;
+//        } elseif ($correctAnswer == normalize($answer)) {
+//            line("Correct!");
+//        } else {
+//            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $correctAnswer);
+//            line('Let\'s try again, %s!', $name);
+//            return;
+//        }
+//    }
+//
+//    line('Congratulations, %s!', $name);
+//    return;
+//}
